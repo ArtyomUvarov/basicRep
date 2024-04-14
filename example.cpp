98283e91a8a6119c20f22cefd06d673d88ed98d9
uvarov_ad <artyom.uvarov@c3dlabs.com>
2024-04-14 08:39:16 +0300

    поместил функцию в отдельный файл

--- example.cpp
+++ example.cpp
@@ -5,3 +5,7 @@
 void example_function() {
 	std::cout << "Hello from example function!" << std::endl;
 }
+
+void todo_move_me_out() {
+	std::cout << "This function should be located in the separated source file" << std::endl;
+}
