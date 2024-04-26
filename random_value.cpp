b3376fdb849e4f1d9796a86f736f462e4f39ba08
uvarov_ad <artyom.uvarov@c3dlabs.com>
2024-04-26 08:33:08 +0300

    main(удалил файлы)

--- random_value.cpp
+++
@@ -1,8 +0,0 @@
-#include "random_value.h"
-
-int random() {
-	std::srand(std::time(nullptr)); // use current time as seed for random generator
-	const int random_value = std::rand() % 100;
-	//std::cout << random_value << std::endl;
-	return random_value;
-}
--- random_value.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <iostream>
-#include <cstdlib>
-#include <ctime>
-
-int random();
