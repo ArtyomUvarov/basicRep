4a0a95594d481ca1e37f924838551bfb446bd7cf
uvarov_ad <artyom.uvarov@c3dlabs.com>
2024-05-18 09:02:09 +0300

    hW4(first)(доработка)

--- Ball.hpp
+++ Ball.hpp
@@ -4,23 +4,23 @@
 #include "Velocity.hpp"
 
 class Ball {
-public:
+private:
   Point    _center; // центр шара
   Velocity _velo;   // скорость шара
-  Color   _color;   // цвет шара
-  double  _rad;     // радиус шара
-  double  _mass;    // масса шара
+  Color    _color;  // цвет шара
+  double   _rad;    // радиус шара
+  double   _mass;   // масса шара
 public:
   Ball( const Point &    center,
         const Velocity & velo,
         const Color  &   color,
               double     rad,
               double     mass );
-    void setVelocity(const Velocity& velocity);
-    Velocity getVelocity() const;
-    void draw(Painter& painter) const;
-    void setCenter(const Point& center);
-    Point getCenter() const;
-    double getRadius() const;
-    double getMass() const;
+   void setVelocity(const Velocity& velocity);
+   Velocity getVelocity() const;
+   void draw(Painter& painter) const;
+   void setCenter(const Point& center);
+   Point getCenter() const;
+   double getRadius() const;
+   double getMass() const;
 };
